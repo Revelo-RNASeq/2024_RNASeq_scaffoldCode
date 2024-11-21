ede92531e44cc7ef071ff21d0f1c9cd805b53f43
Marco Chiapello <chiapello.m@gmail.com>
2024-11-21 19:53:56 +0100

    file 1

--- 001_a_sec-preproc_import.R
+++ 001_a_sec-preproc_import.R
@@ -2,23 +2,24 @@
 # Il primo passo è creare la cartella chiamata "data" usando il pacchetto fs (https://fs.r-lib.org/) 
 
 # Caricare la library chiamata fs 
-library(__)
+library(fs)
 
 # Creare la cartella "data"
-___("data")
+dir_create("data")
 
 
 # Scaricare i file che ci serviranno per l'analisi 
-# 1. GSE96870_counts_cerebellum.csv
+# 1. GSE96870_counts.csv
 # 2. GSE96870_coldata_all.csv
-# I file sono tutti in un repository su github: https://github.com/carpentries-incubator/bioc-rnaseq/raw/main/episodes/data/
+# I file sono tutti in un repository su github: https://github.com/Revelo-RNASeq/2024_RNASeq_scaffoldCode/raw/data/
+
 
 download.file(
-  url = "https://github.com/carpentries-incubator/bioc-rnaseq/raw/main/episodes/data/___", 
-  destfile = "data/___"
+  url = "https://github.com/Revelo-RNASeq/2024_RNASeq_scaffoldCode/raw/data/GSE96870_counts.csv", 
+  destfile = "data/GSE96870_counts.csv"
   )
 
 download.file(
-  url = "___", 
-  destfile = "___"
+  url = "https://github.com/Revelo-RNASeq/2024_RNASeq_scaffoldCode/raw/data/GSE96870_coldata_all.csv", 
+  destfile = "data/GSE96870_coldata_all.csv"
 )
